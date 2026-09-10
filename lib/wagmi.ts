a3e1015a9960193158de59f202ff031e95da172f
wlylabs <wly38347@gmail.com>
2026-09-10 21:03:41 +0700

    Update wagmi.ts

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -1,9 +1,10 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { mainnet, base, polygon } from "wagmi/chains";
+import { mainnet, base } from "wagmi/chains";
+import { robinhoodChain } from "./chains";
 
 export const wagmiConfig = getDefaultConfig({
   appName: "Nexa Chain",
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-  chains: [mainnet, base, polygon],
+  chains: [robinhoodChain, mainnet, base],
   ssr: true,
 });
