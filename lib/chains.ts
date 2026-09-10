2f5856b38ecb40a888bcf8f757e648ea34a76863
wlylabs <wly38347@gmail.com>
2026-09-10 21:04:10 +0700

    Create lib/chains.ts

---
+++ lib/chains.ts
@@ -0,0 +1,16 @@
+import { defineChain } from "viem";
+
+export const robinhoodChain = defineChain({
+  id: 4663,
+  name: "Robinhood Chain",
+  nativeCurrency: { name: "Ether", symbol: "ETH", decimals: 18 },
+  rpcUrls: {
+    default: { http: ["https://rpc.mainnet.chain.robinhood.com"] },
+  },
+  blockExplorers: {
+    default: {
+      name: "Robinhood Chain Explorer",
+      url: "https://robinhoodchain.blockscout.com",
+    },
+  },
+});
